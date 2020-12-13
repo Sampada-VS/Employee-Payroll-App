1a1b7edddd0af1713221c9f08353b34442e5ff02
Sampada-VS <samssv1@gmail.com>
2020-12-14 01:29:52 +0530

    Added: for converting into client server-step2.retrieve employee payroll data from JSON server instead local storage

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,5 +1,5 @@
 let site_properties={
-    use_local_storage:"true",
+    use_local_storage:"false",
     home_page:"../pages/employee_payroll_home.html",
     form_page:"../pages/employee_payroll_form.html",
     server_url:"http://127.0.0.1:3000/EmployeePayrollDB/"
